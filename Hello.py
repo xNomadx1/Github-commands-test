2f64cc871b5656dbf99fd8f8665acc795b6a79d1
Tyler <tearnhardt71@outlook.com>
2025-10-16 13:21:32 -0700

    Small intro

--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
 print("Hello, Github world!")
 
-print("My name is Tyler!")
+name = "tyler nomadx"
+print(f"My name is {name.title()}.")
+print("I'm learning about Python progamming & source control.")
